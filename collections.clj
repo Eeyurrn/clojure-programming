d38ef46d827832267e1b3791f1b8f45da25743b2
Eeyurrn <eeyurrn@eeyurrn.com>
2015-09-01 22:04:32 +1000

    add stuff about lazy seqs

--- collections.clj
+++ collections.clj
@@ -62,4 +62,103 @@
 (map-map inc (hash-map :z 5 :c 6 :a 0))
 
 
-;;
+;;Counting various collections
+(count [1 1 3])
+
+(count {:a 1 :b 2 :c 3})
+
+(count #{1 2 3})
+
+(count '(1 2 3))
+
+;;seqs
+(seq "Clojure")
+
+(seq {:a 5 :b 6})
+
+
+(seq (java.util.ArrayList. (range 5)))
+
+
+(seq (into-array ["Clojure" "Programming"]))
+
+(seq [])
+
+(seq nil)
+
+;;Map function implicitly converts the String into a seq
+(map str "Clojure")
+
+;;So does the set function.
+
+(set "Programming")
+
+;;first rest and next
+
+(first "Clojure")
+
+(rest "Clojure")
+
+(next "Clojure")
+
+;;rest vs next
+(rest [1])
+
+(next [1])
+
+;;rest will always return an empty seq and not nil
+
+(rest nil)
+
+(next nil)
+
+(doseq [x (range 3)]
+  (println x))
+
+(let [r (range 3)
+      rst (rest r)]
+  (prn (map str rst))
+  (prn (map #(+ 100 %) r))
+  (prn (conj r -1)(conj rst 42)))
+
+
+;;Lazy eval vs list
+;;lazy
+(let [s (range 1e6)]
+  (time (count s)))
+
+;;un-lazy
+(let [s (apply list (range 1e6))]
+  (time (count s)))
+
+;;cons example
+(cons 0 (range 1 5))
+
+(cons :a [:b :c :d])
+
+(cons 0 (cons 1 (cons 2 (cons 3 (range 4 10)))))
+
+(list* 0 1 2 3 (range 4 10))
+
+
+(defn random-ints
+  "Returns a lazy seq of random integers in the range [0,limit"
+  [limit]
+  (lazy-seq
+   (println "realizing random number")
+   (cons (rand-int limit)
+         (random-ints limit))))
+
+(take 10 (random-ints 50))
+
+(def rands (take 10 (random-ints 50)))
+;;Lazy seq will evaluate
+(first rands)
+;; keeps evaluating
+(nth rands)
+;;will keep the previous values and will continue from where it last evaluated
+(count rands)
+
+
+
+
