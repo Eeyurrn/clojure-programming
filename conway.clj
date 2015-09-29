9a45b55a627f7e241a263abdac70ea3554b38a3d
Eeyurrn <iansergeanttan@gmail.com>
2015-09-29 14:56:27 +1000

    add the rest of the methods to process a step in the GOL

--- conway.clj
+++ conway.clj
@@ -14,3 +14,23 @@
 (def glider (populate (empty-board 6 6) #{[2 0] [2 1] [2 2] [1 2] [0 1]}))
 
 (pprint glider)
+
+(defn neighbours ;;gets the neighbour cell for a set of coordinates
+  [[x y]]
+  (for [dx [-1 0 1] dy [-1 0 1] :when (not= 0 dx dy)]
+    [(+ dx x)(+ dy y)]))
+
+
+
+(defn count-neighbours
+  [board loc]
+  (count (filter #(get-in board %)(neighbours loc))))
+
+(defn indexed-step
+  "Yields the next state of the board, using indices to determine neighbours, liveness, etc."
+  [board]
+  (let [w (count board)
+        h (count (first board))]
+   (loop [new-board board x 0 y 0]
+    (cond
+     (>= x w) new-board))))
