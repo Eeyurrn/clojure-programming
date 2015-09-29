eb0f523582b80ed505573e7c22d3f4ace8224ffa
Eeyurrn <iansergeanttan@gmail.com>
2015-09-29 15:07:44 +1000

    finish the liveness function

--- conway.clj
+++ conway.clj
@@ -33,4 +33,14 @@
         h (count (first board))]
    (loop [new-board board x 0 y 0]
     (cond
-     (>= x w) new-board))))
+     (>= x w) new-board
+     (>= y h) (recur new-board (inc x) 0)
+     :else
+     (let [new-liveness
+           (case (count-neighbours board [x y])
+             2 (get-in board [x y])
+             3 :on
+             nil)]
+       (recur (assoc-in new-board [x y] new-liveness x (inc y))))))))
+
+(-> (iterate indexed-step glider) (nth 8) pprint)
