dbac64ffff47ca2aa0b8ee98a94bb7ceefd8ebf2
Eeyurrn <iansergeanttan@gmail.com>
2015-10-07 23:00:16 +1100

    start on map destructuring work

--- destructuring.clj
+++ destructuring.clj
@@ -27,3 +27,33 @@
 ;;variable arguments
 (let [[x & rest] v];; use the & character to define the varargs at the end to the rest form
   rest)
+
+;;retaining the original value using :as
+
+(let [[x _ z :as original-vector] v]
+  (conj original-vector (+ x z)))
+
+;;Map destructuring
+
+(def m {:a 5 :b 6
+        :c [7 8 9]
+        :d {:e 10 :f 11}
+        "foo" 88
+        42 false})
+
+;;visually mirror the input map
+(let [{a :a b :b} m]
+  (+ a b))
+
+;;any value may be used for lookup, using "foo"
+(let [{f "foo"} m]
+  (+ f 12))
+
+;;same with 42
+(let [{v 42} m]
+  (if v 1 0))
+
+;;can use the map destructuring on a vector due to the associative abstraction
+;; treats vector like a map with integers as keys
+(let [{x 3 y 8}[12 0 0 -18 44 6 0 0 1]]
+  (+ x y))
